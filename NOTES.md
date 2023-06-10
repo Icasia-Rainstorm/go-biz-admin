5aadba0c72483df7ad7be713ae315eec214dd47f
mousepotato <paradise.lsj@gmail.com>
2023-06-10 00:23:49 -0700

    update

--- NOTES.md
+++ NOTES.md
@@ -14,4 +14,21 @@
 
 - add DB
 
+- git tag 1
+
 ## Now create package for database and routes
+
+
+- git tag: 2
+
+
+## Now create and register user
+
+- create users (dummy)
+- live reloading using https://github.com/cosmtrek/air
+
+```
+# binary will be $(go env GOPATH)/bin/air
+curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
+```
+- then start with `air`
--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mousepotato/go-biz-admin/controllers"
 	"net/http"
 )
 
@@ -12,10 +13,12 @@ func SetupRouter() *gin.Engine {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello World!")
 	})
-
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
+
+	// User Handlers
+	r.POST("/api/register", controllers.Register)
 
 	return r
 }
